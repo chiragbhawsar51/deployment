240a5ec7d5e640964b2344e22613a59390888bb2
chiragbhawsar51 <112650505+chiragbhawsar51@users.noreply.github.com>
2024-06-21 16:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,7 @@ FINAL_FILE_DOCX_FILENAME = "Final_Cover_letter_with_table_{}.docx"
 FINAL_FILE_PDF_FILENAME = "Final_Cover_letter_with_table_{}.pdf"
 PDFS_DIRECTORY = os.path.join(app.root_path, 'pdfs')
 
-# Path to wkhtmltopdf executable
-wkhtmltopdf_path = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Adjust this path according to your installation
+wkhtmltopdf_path = '/usr/bin/wkhtmltopdf  # Adjust this path according to your installation
 
 # Configure pdfkit with wkhtmltopdf path
 pdfkit_config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
